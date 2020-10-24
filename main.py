09d065e32356cda358026f3c312aeb3a4252591a
Ben Madelin <ben.madelin@ntlworld.com>
2020-10-24 05:42:57 +0100

    Cipher Algorithm will increment through provided message and manipulate the given char value, shifting it in a round robin method.

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ def menu():
                     print("You must input your Student Identity number! ")
                     break
                 else:
-                    encrypt()
+                    cipherMsg("encrypt")
             elif userInput == 3:  # Decrypt
                 if KEY is None:
                     print("You must input your Student Identity number! ")
                     break
                 else:
-                    decrypt()
+                    cipherMsg("decrypt")
             else:  # Exit
                 while True:
                     exitProgram = input("Are you sure you want to exit the program Y/N: ")
@@ -62,12 +62,50 @@ def setKey():
     menu()
 
 
-def encrypt():
-    print("ENCRYPT")
+def cipherMsg(operation):
 
+    global KEY
 
-def decrypt():
-    print("DECRYPT")
+    shiftValue = [int(x) for x in str(KEY)]
+
+    charList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+                "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f",
+                "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ",
+                "."]
+
+    oldMessage = str(input("Enter message: "))
+    newMessage = []
+
+    incValue = 0
+    try:
+        for char in oldMessage:
+            charIndex = charList.index(char)
+
+            if incValue > 5:
+                incValue = 0
+
+            if operation == "encrypt":
+                charIndex = charIndex + shiftValue[incValue]
+            elif operation == "decrypt":
+                charIndex = charIndex - shiftValue[incValue]
+
+            if charIndex > 64:
+                charIndex = charIndex % 64
+            elif charIndex < 1:
+                charIndex = 64 - ((charIndex * -1) % 64)
+
+            newMessage.append(charList[charIndex])
+
+            incValue = incValue + 1
+
+        message = "".join(newMessage)
+
+        print("Your " + operation + "ed Message: " + message)
+
+    except ValueError:
+        print("The message contains an invalid character! \nONLY lowercase/uppercase, digits, period and space characters are accepted! ")
+
+    menu()
 
 
 menu()
